6eb23a08bb6e95c83de53420bc48c5ccfc5e23b4
gonzalo <meru.vi06@gmail.com>
2019-01-15 09:55:43 -0400

    UPDATE SERVICE NOTA (mostrar logs para mejorar el seguimiento de la aplicacion)

--- src/main/java/com/notas/core/service/NotaService.java
+++ src/main/java/com/notas/core/service/NotaService.java
@@ -2,6 +2,8 @@ package com.notas.core.service;
 
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -23,40 +25,54 @@ public class NotaService {
 	@Qualifier("convertidor")
 	private Convertidor convertidor;
 	
+	private static final Log logger = LogFactory.getLog(NotaService.class);
+	
 	public boolean crear(Nota nota) {
+		logger.info("CREANDO NOTA");
 		try {
 			repositorio.save(nota);
+			logger.info("NOTA CREADA");
 			return true;
 		}catch(Exception ex) {
+			logger.error("HUBO UN ERROR AL CREAR NOTA");
 			return false;
 		}
 	}
 	
 	public boolean actualizar(Nota nota) {
+		logger.info("ACTULIZANDO NOTA");
 		try {
 			repositorio.save(nota);
+			logger.info("NOTA ACTUALIZADA");
 			return true;
 		}catch(Exception ex) {
+			logger.error("HUBO UN ERROR AL ACTUALIZAR NOTA");
 			return false;
 		}
 	}
 	
 	public boolean borrar(long id) {
+		logger.info("BORRANDO NOTA");
 		try {
 			Nota nota = repositorio.findById(id);
 			repositorio.delete(nota);
+			logger.info("NOTA BORRADA");
 			return true;
 		}catch(Exception ex) {
+			logger.error("HUBO UN ERROR AL BORRAR NOTA");
 			return false;
 		}
 	}
 	
 	public boolean borrar(String nombre, long id) {
+		logger.info("BORRANDO NOTA");
 		try {
 			Nota nota = repositorio.findByNombreAndId(nombre, id);
 			repositorio.delete(nota);
+			logger.info("NOTA BORRADA");
 			return true;
 		}catch(Exception ex) {
+			logger.error("HUBO UN ERROR AL BORRAR NOTA");
 			return false;
 		}
 	}
@@ -71,8 +87,10 @@ public class NotaService {
 	}
 	
 	public List<MNota> obtener(){
+		logger.info("LISTANDO NOTA");
 		List<MNota> notas  = null;
 		notas = convertidor.convertirLista(repositorio.findAll());
+		logger.info("NOTA LISTADA");
 		return notas;
 	}
 }
