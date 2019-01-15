3c1a3bab40b5ea0fdafd7304ae519b83a255bd2c
gonzalo <meru.vi06@gmail.com>
2019-01-14 21:39:38 -0400

    APP-PROPERTIES, MODEL MNOTA, ENTITY NOTA Y AL CORRER CREA LA TABLA NOTA CON SUS CAMPOS EN LA BASE DE DATOS

--- src/main/java/com/notas/core/RestFullApplication.java
+++ src/main/java/com/notas/core/RestFullApplication.java
@@ -1,4 +1,4 @@
-package com.restfull.RESTFull;
+package com.notas.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/notas/core/model/MNota.java
@@ -0,0 +1,68 @@
+package com.notas.core.model;
+
+import com.notas.core.entity.Nota;
+
+public class MNota {
+	
+	private long id;
+	
+	private String nombre;
+	
+	private String titulo;
+	
+	private String contenido;
+
+	public MNota() {
+		
+	}
+	
+	public MNota(Nota nota) {
+		this.id = nota.getId();
+		this.nombre = nota.getNombre();
+		this.titulo = nota.getTitulo();
+		this.contenido = nota.getContenido();
+	}
+	
+	public MNota(int id, String nombre, String titulo, String contenido) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.titulo = titulo;
+		this.contenido = contenido;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getContenido() {
+		return contenido;
+	}
+
+	public void setContenido(String contenido) {
+		this.contenido = contenido;
+	}
+	
+	
+	
+}
